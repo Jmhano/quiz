89f43665716c088e687bfd26a71b42a5857269d0
Jmhano <jmhano@gmail.com>
2022-07-07 18:46:34 -0400

    added alert to submit button

--- Assets/js/score.js
+++ Assets/js/score.js
@@ -1,2 +1,6 @@
 var mostRecentScore = localStorage.getItem("mostRecentScore");
-finalScore.innerText= mostRecentScore;
+finalScore.innerText = mostRecentScore;
+
+document.getElementById("submit").addEventListener("click", function () {
+  window.alert("Thank you for taking this quiz!");
+});
